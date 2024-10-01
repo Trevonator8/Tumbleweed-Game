cb1d968a409dc764176e11979e9befc624146c25
Trevor Christian <tchristian@alaska.edu>
2024-10-01 12:57:26 -0800

    Added some textures for the ground and made a background for TitleScreen

--- tumbleweed/TitleScreen.gd
+++ tumbleweed/TitleScreen.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
 	pass # Replace with function body.
 
 
