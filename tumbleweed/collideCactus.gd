76986c3c1c811c813c362419641343c96e6b1d7d
Mary <mhb93@nau.edu>
2024-10-12 14:36:38 -0800

    Fixed cacti and appropriate spikes appearing

--- tumbleweed/collideCactus.gd
+++ tumbleweed/collideCactus.gd
@@ -6,25 +6,55 @@ func _ready() -> void:
 	#var cactusSelector = randomNumber.randf_range(0, 4)
 	var cactusSelector = randi_range(1, 4)
 	if cactusSelector == 1:
-		$"../../Cactus2/Cylinder_002".visible = true  # Hides the cactus's mesh
+		$"../../Cactus2/Cylinder_002".visible = true  # Shows the cactus's mesh
+		$"../../Cactus2/Cone_030".visible = true
+		
 		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cone".visible = false
+		
 		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cone".visible = false
+		
 		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cone_032".visible = false
+		
 	if cactusSelector == 2:
 		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus2/Cone_030".visible = false
+		
 		$"../../Cactus3/Cylinder".visible = true  # Hides the cactus's mesh
+		$"../../Cactus3/Cone".visible = true
+
 		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cone".visible = false
+		
 		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cone_032".visible = false
+		
 	if cactusSelector == 3:
 		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus2/Cone_030".visible = false
+		
 		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cone".visible = false
+		
 		$"../../Cactus4/Cylinder_002".visible = true  # Hides the cactus's mesh
+		$"../../Cactus4/Cone".visible = true
+		
 		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cone_032".visible = false
 	if cactusSelector == 4:
 		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus2/Cone_030".visible = false
+		
 		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cone".visible = false
+		
 		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cone".visible = false
+
 		$"../../Cactus5/Cylinder_002".visible = true  # Hides the cactus's mesh
+		$"../../Cactus5/Cone_032".visible = true
 
 func _on_body_entered(body: Node3D) -> void:
 	print("body enter")
