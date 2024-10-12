ac94c965453cb20f63675634cf9c2e4b6e25678c
Mary <mhb93@nau.edu>
2024-10-12 13:53:43 -0800

    Cacti bodles are randomly chosen and displayed
    Thorns need work

--- tumbleweed/collideCactus.gd
+++ tumbleweed/collideCactus.gd
@@ -3,15 +3,28 @@ extends Area3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-func _on_area_3d_body_entered(body) -> void:
-	print("3d body enter")
-	#if body.name == "tumbleweedmodel":  # Replace with your tumbleweed's name
-		#$MeshInstance3D.visible = false  # Hides the flower's mesh
-		#queue_free()  # Optionally remove the flower completely
-
+	#var cactusSelector = randomNumber.randf_range(0, 4)
+	var cactusSelector = randi_range(1, 4)
+	if cactusSelector == 1:
+		$"../../Cactus2/Cylinder_002".visible = true  # Hides the cactus's mesh
+		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+	if cactusSelector == 2:
+		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cylinder".visible = true  # Hides the cactus's mesh
+		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+	if cactusSelector == 3:
+		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cylinder_002".visible = true  # Hides the cactus's mesh
+		$"../../Cactus5/Cylinder_002".visible = false  # Hides the cactus's mesh
+	if cactusSelector == 4:
+		$"../../Cactus2/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus3/Cylinder".visible = false  # Hides the cactus's mesh
+		$"../../Cactus4/Cylinder_002".visible = false  # Hides the cactus's mesh
+		$"../../Cactus5/Cylinder_002".visible = true  # Hides the cactus's mesh
 
 func _on_body_entered(body: Node3D) -> void:
 	print("body enter")
